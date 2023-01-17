ea43f9ee02902e0fc4c4f7360b7ccea37bcfa3e1
kumar2014 <kumaravel.au@gmail.com>
2023-01-17 14:47:10 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,26 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs';
-
-import { Product } from './model/products';
-import { ProductService } from './Service/products.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'AngularHttpRequest';
-  
-
-  constructor(private http:HttpClient) {}
-
-  ngOnInit() {
-    
-  }
-
- 
-
+  // #8
+  constructor(private http: HttpClient) {}
+  // #9   onProductCreate(products: This products going to an ngform object
   onProductCreate(products: { pName: string; desc: string; price: string }) {
     console.log(products);
-    this.http.post()
+    const headers = new HttpHeaders({ myHrader: 'procademy' });
+    this.http
+      .post(
+        'https://angularbyprocademy-default-rtdb.firebaseio.com/products.json',
+        products,
+        { headers }
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
-
-  
-
- 
 }
